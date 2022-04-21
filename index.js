84f5e2a61238639e042cada91c624003b33b8b32
NodjaLima <nodjamplima@gmail.com>
2022-04-21 08:42:44 -0300

    corrige bug na variavel hora

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function carregar () {
   var msg = window.document.getElementById('msg');
   var img = window.document.getElementById('imagem');
   var data = new Date();
-  var hora = 15
-  //var hora = data.getHours();
+  //var hora = 15
+  var hora = data.getHours();
   msg.innerHTML = `<strong>Agora são ${hora} horas.<strong>`
   if  (hora >= 0 && hora < 12) {
     //Bom dia
